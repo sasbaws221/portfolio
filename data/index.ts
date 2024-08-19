5099db9e45653a36bd75606f02329a5809ea0f76
sasbaws221 <sasbaws22@gmail.com>
2024-08-19 08:07:38 -0700

    first commit

---
+++ data/index.ts
@@ -0,0 +1,198 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/carepulse-1.jpg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+  {
+    id: 5,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Aura-AI Video Sharing Mobile Platform",
+    des: "Explore the imaginative beauties AI powered mobile app can provide.This mobile application is a one stop platform.",
+    img: "/aora-1.jpg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg",],
+    link: "https://github.com/sasbaws221/Aura.git",
+  },
+  {
+    id: 2,
+    title: "Carepulse-Hospital Appointment Platform",
+    des: "Simplify your appointment with your favourite Hospital.This application makes it possible",
+    img: "/carepulse-1.jpg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/c.svg"],
+    link: "https://github.com/sasbaws221/Hmanagementapp.git",
+  },
+  {
+    id: 3,
+    title: "AI Image SaaS - Canva Application",
+    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg",],
+    link: "https://github.com/sasbaws221/AI-Generator-SAAS-Platform.git",
+  },
+  {
+    id: 4,
+    title: "Horizon-Fintech Application",
+    des: "Redefining the bnking and financial applications in the NEXT level..",
+    img: "/horizon-1.jpg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", ],
+    link: "https://github.com/sasbaws221/myBankApp.git",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Samuel was an absolute pleasure. If you're seeking to elevate your website and elevate your brand, Samuel is the ideal partner.",
+    name: "",
+    title: "CEO of Appsoft System Consult.",
+  },
+  {
+     quote:
+      "Collaborating with Samuel was an absolute pleasure. If you're seeking to elevate your website and elevate your brand, Samuel is the ideal partner.",
+    name: "",
+      title: "CEO of Appsoft System Consult.",
+  },
+  {
+    quote:
+      "Collaborating with Samuel was an absolute pleasure. If you're seeking to elevate your website and elevate your brand, Samuel is the ideal partner.",
+    name: "",
+    title: "CEO of Appsoft System Consult.",
+  },
+  {
+    quote:
+      "Collaborating with Samuel was an absolute pleasure. If you're seeking to elevate your website and elevate your brand, Samuel is the ideal partner.",
+    name: "",
+    title: "CEO of Appsoft System Consult.",
+  },
+  {
+     quote:
+      "Collaborating with Samuel was an absolute pleasure. If you're seeking to elevate your website and elevate your brand, Samuel is the ideal partner.",
+    name: "",
+    title: "CEO of Appsoft System Consult.",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+ 
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev ",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link:"https://sasbaws221.git"
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link:"https://x.com/samuel"
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link:"https://www.linkedin.com/in/Appsoft-system-consult"
+  },
+];
